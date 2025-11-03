dfcfa2206355e79a07fa569300737c44176e2887
Jasonlgrd <jlagarde@semeia.io>
2025-11-04 00:45:56 +0100

    ⚛️ Init Routes and Home in main.jsx

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,10 +1,12 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router";
 import "./index.css";
-import App from "./App.jsx";
+import Athletes from "./Athletes.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <Routes>
+      <Route path="/" element={<Athletes />} />
+    </Routes>
   </BrowserRouter>
 );
