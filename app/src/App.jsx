1bf53b841fe5d0d94938c13ffbf80e35c1f01ee8
Jasonlgrd <jlagarde@semeia.io>
2025-11-06 23:45:03 +0100

    ⚛️ Add Toaster in App component

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -3,16 +3,20 @@ import Login from "./Login.jsx";
 import Athletes from "./Athletes.jsx";
 import Athlete from "./Athlete.jsx";
 import AthleteEdit from "./AthleteEdit.jsx";
+import { Toaster } from "./ui/Toaster.jsx";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Athletes />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/athlete/:id" element={<Athlete />} />
-      <Route path="/athlete/:id/edit" element={<AthleteEdit />} />
-      <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
+    <>
+      <Toaster />
+      <Routes>
+        <Route path="/" element={<Athletes />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/athlete/:id" element={<Athlete />} />
+        <Route path="/athlete/:id/edit" element={<AthleteEdit />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </>
   );
 }
 
