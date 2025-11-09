f1e32c1693ded29f2fe2bec6365b24a868fc93d4
Jasonlgrd <jlagarde@semeia.io>
2025-11-09 10:43:23 +0100

    ⚡️ Add seed

---
+++ supabase/seed.sql
@@ -0,0 +1,16 @@
+insert into public.athletes (
+        last_name,
+        first_name,
+        birthdate,
+        location,
+        avatar
+    )
+values ('Laidlow', 'Sam', '1998-12-23', 'Oakley', null),
+    (
+        'Blummenfelt',
+        'Kristian',
+        '1994-02-14',
+        'Bergen',
+        null
+    ),
+    ('Iden', 'Gustav', '1996-05-01', 'Bergen', null);
